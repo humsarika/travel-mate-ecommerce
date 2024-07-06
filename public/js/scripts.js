ff5341552152de141320228aace02c88a2e6f22b
Sarika Kushwaha <sarika.vsa01@gmail.com>
2024-07-06 13:51:46 +0530

    add some minor changes for responsivesness

---
+++ public/js/scripts.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const menu = document.querySelector('.menu');
+    menu.classList.toggle('active');
+}
+function toggleCategoriesMenu() {
+    const categoriesLinks = document.querySelector('.categories-links');
+    categoriesLinks.classList.toggle('active');
+  }
