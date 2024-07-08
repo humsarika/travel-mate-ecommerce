2da4b8e4cad994202f890ce8c41d88ff7624628a
Sarika Kushwaha <sarika.vsa01@gmail.com>
2024-07-08 16:49:13 +0530

    reverting back to - add some minor changes for responsivesness

--- public/js/scripts.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    const menu = document.querySelector('.menu');
-    menu.classList.toggle('active');
-}
-function toggleCategoriesMenu() {
-    const categoriesLinks = document.querySelector('.categories-links');
-    categoriesLinks.classList.toggle('active');
-  }
